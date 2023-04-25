18cfaa9368637d2f556c696480326048b5fe6810
Daniel Klauck <daniel_klauck@hotmail.com>
2023-04-25 00:43:50 -0300

    Ajustes e comentários

--- dft_daniel_diego.py
+++ dft_daniel_diego.py
@@ -1,33 +1,54 @@
-
+#Alunos Daniel Klauck e Diego Arruda
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def dft(x):
-    Xsize = len(x)
-    X = np.zeros(Xsize, dtype=complex)
+    
+    Xsize = len(x)#eixo Xsize tem o tamanho do vetor de dados x
 
-    for m in range(Xsize):
-        mysumm = 0
+    X = np.zeros(Xsize, dtype=complex) #cria um array de zeros com o tipo de dados complexos
+    
+    for m in range(Xsize): #percorre os valores de m de 0 a Xsize-1
+
+        mysumm = 0 #para cada valor de m, a soma deve ser reinicializada
+
         for n in range(Xsize):
-            mysumm += x[n] * (np.cos(2*np.pi*m*n/Xsize) -
-                              1j*np.sin(2*np.pi*m*n/Xsize))
+                        
+            # calcula a parte real da DFT
+            re = np.cos(2 * np.pi * m * n / Xsize)
+
+            # calcula a parte imaginária da DFT
+            im = -1j * np.sin(2 * np.pi * m * n / Xsize)
+            
+            # soma o termo real e imaginário
+            mysumm += x[n] * (re + im)
+
         X[m] = mysumm
 
     return X
 
-
-x = np.linspace(0, 2*np.pi, 100)
-y = np.sin(x)
+x = np.linspace(0, 2*np.pi, 100) #x é o numero de amostras# retorna numeros uniformemente espaçados
+y = np.sin(x) #calcula y para plotar no gráfico
 X = dft(y)
 
-fig, axs = plt.subplots(2)
-axs[0].plot(x, y)
-axs[0].set_title('Sinal original')
-axs[1].plot(np.arange(len(X)), np.abs(X))
-axs[1].set_title('Magnitude da Transformada de Fourier')
-plt.show()
+fig, axs = plt.subplots(3, 1, figsize=(8, 8))
 
+axs[0].plot(x, y)
+axs[0].set_xlabel('Tempo')
+axs[0].set_ylabel('Amplitude')
+axs[0].set_title('Sinal original')
+
+axs[1].plot(np.arange(len(X)), np.abs(X))
+axs[1].set_xlabel('Frequência')
+axs[1].set_ylabel('Magnitude')
+axs[1].set_title('Magnitude da Transformada de Fourier')
+
+axs[2].plot(np.arange(len(X)), np.angle(X))
+axs[2].set_xlabel('Frequência')
+axs[2].set_ylabel('Ângulo (radianos)')
+axs[2].set_title('Fase')
+
+plt.show()
 
 '''
 # Exemplo de uso
@@ -47,10 +68,10 @@ plt.plot(np.angle(X))
 plt.title('Fase')
 plt.xlabel('Frequência')
 plt.ylabel('Ângulo (radianos)')
-plt.show()
+plt.show()'''
 
 
-
+'''
 function[X] = dft(x)
 Xsize = length(x);
 
